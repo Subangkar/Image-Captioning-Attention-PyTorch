4890514991f9a13bc9bdfd5fd708400816d47dc0
Subangkar <subangkar.karmaker@gmail.com>
2020-12-02 11:41:22 +0600

    flickr8k image ds loaded like coco14

---
+++ datasets/flickr8k.py
@@ -0,0 +1,57 @@
+from collections import defaultdict
+
+
+class Flickr8k:
+    capt_path = 'Flickr8k_text/Flickr8k.token.txt'
+    img_path = 'Flicker8k_Dataset/'
+    dist_detail_path = {
+        'train': 'Flickr8k_text/Flickr_8k.trainImages.txt',
+        'valid': 'Flickr8k_text/Flickr_8k.devImages.txt',
+        'test': 'Flickr8k_text/Flickr_8k.testImages.txt',
+    }
+
+    def __init__(self, base_dataset_path='data/flickr8k/', dist_type='valid'):
+        img_id_cap_map = self.image_to_caption_dict(image_path=base_dataset_path + Flickr8k.img_path,
+                                                    tokens_path=base_dataset_path + Flickr8k.capt_path)
+        ids = set(img_id_cap_map.keys())
+        id_to_file = defaultdict()
+        for i, (img_id, capt) in enumerate(img_id_cap_map.items()):
+            # id_to_file.append(base_dataset_path + Flickr8k.img_path + img_id + '.jpg')
+            id_to_file[img_id] = base_dataset_path + Flickr8k.img_path + img_id + '.jpg'
+
+        self.ids = list(ids)
+        self.id_to_file = id_to_file
+        self.id_to_captions = img_id_cap_map
+
+    def image_to_caption_dict(self, image_path, tokens_path):
+        with open(tokens_path) as f:
+            data = f.read()
+
+        descriptions = dict()
+
+        for el in data.strip().split("\n"):
+
+            tokens = el.split()
+            image_id, image_desc = tokens[0], tokens[1:]
+
+            # dropping .jpg from image id
+            image_id = image_id.split(".")[0]
+
+            image_desc = " ".join(image_desc)
+
+            if image_id in descriptions:
+                descriptions[image_id].append(image_desc)
+            else:
+                descriptions[image_id] = [image_desc]
+
+        return descriptions
+
+    def __getitem__(self, index: int) -> (str, list):
+        """
+        :param index:
+        :return: (img_file_path, list_of_captions)
+        """
+        return self.id_to_file[self.ids[index]], self.id_to_captions[self.ids[index]]
+
+    def __len__(self):
+        return len(self.ids)
