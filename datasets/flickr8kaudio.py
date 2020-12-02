9a528f2cc0a0ea4f6afe63f4be8f9d0dc084cdce
Masum <1505014.mr@ugrad.cse.buet.ac.bd>
2020-12-02 14:59:58 +0600

    flickr8k dict build complete

--- datasets/flickr8kaudio.py
+++ datasets/flickr8kaudio.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+import re 
 
 
 class Flickr8kAudio:
@@ -12,47 +13,86 @@ class Flickr8kAudio:
     audio_path = 'flickr_audio/wavs/'
 
     def __init__(self, base_dataset_path='data/flickr8k/', dist_type='valid'):
-        img_id_cap_map = self.image_to_caption_dict(image_path=base_dataset_path + Flickr8kAudio.img_path,
-                                                    captn_path=base_dataset_path + Flickr8kAudio.capt_path)
-        ids = set(img_id_cap_map.keys())
-        id_to_file = defaultdict()
-        for i, (img_id, capt) in enumerate(img_id_cap_map.items()):
-            # id_to_file.append(base_dataset_path + Flickr8kAudio.img_path + img_id + '.jpg')
-            id_to_file[img_id] = base_dataset_path + Flickr8kAudio.img_path + img_id + '.jpg'
 
-        self.ids = list(ids)
-        self.id_to_file = id_to_file
-        self.id_to_captions = img_id_cap_map
 
-    def image_to_caption_dict(self, image_path, captn_path):
-        with open(captn_path) as f:
+        
+        self.base_dataset_path = base_dataset_path
+        ids_to_filter = None
+        id_file = None
+        if dist_type == 'train':
+            id_file = base_dataset_path  + Flickr8kAudio.dist_detail_path['train']
+        elif dist_type == 'test':
+            id_file = base_dataset_path  + Flickr8kAudio.dist_detail_path['test']
+        elif dist_type == 'valid':
+            id_file = base_dataset_path  + Flickr8kAudio.dist_detail_path['valid']
+        else:
+            print('provide valid type')
+            
+        with open(id_file) as f:
+            ids_to_filter = set([item.split('.')[0]  for item in f.read().splitlines() if len(item)!=0])
+
+        self.caption_count_per_image = 5
+
+        self.dictionary = dict()
+        self.buid_dict(token_path= self.base_dataset_path + Flickr8kAudio.capt_path )
+        
+        tmp_dict = dict()
+        for id in ids_to_filter:
+            tmp_dict[id] = self.dictionary[id]
+        self.dictionary = tmp_dict
+        
+        self.ids = list(self.dictionary.keys())
+        
+
+
+    def buid_dict(self, token_path):
+        with open(token_path,"r") as f:
+
             data = f.read()
 
+        calc_img_path = self.base_dataset_path + Flickr8kAudio.img_path
+        cal_wav_path = self.base_dataset_path + Flickr8kAudio.audio_path
+        print(calc_img_path)
         descriptions = dict()
+        
+        try:
+            for el in data.split("\n"):
+                tokens = el.split()
+                if len(tokens) < 2: continue
+                image_id , image_desc = tokens[0],tokens[1:]
 
-        for el in data.strip().split("\n"):
+                # dropping .jpg from image id
+                image_id = image_id.split(".")[0]
+                image_desc.insert(0, 'startseq') 
+                image_desc.append( 'endseq') 
 
-            tokens = el.split()
-            image_id, image_desc = tokens[0], tokens[1:]
+                image_desc = " ".join(image_desc)
+                image_desc = re.sub(r'[^\w\s]','',image_desc)
 
-            # dropping .jpg from image id
-            image_id = image_id.split(".")[0]
+                if image_id in descriptions:
+                    descriptions[image_id]['descs'].append(image_desc)
+                else:
+                    descriptions[image_id] = dict()
+                    descriptions[image_id]['descs'] = list()
+                    descriptions[image_id]['descs'].append(image_desc)
+                    descriptions[image_id]['im_path'] = calc_img_path + tokens[0].split("#")[0]
+                    descriptions[image_id]['wavs'] = [ cal_wav_path + image_id + '_' + str(cap_id) + '.wav' for cap_id in range(self.caption_count_per_image) ] 
 
-            image_desc = " ".join(image_desc)
 
-            if image_id in descriptions:
-                descriptions[image_id].append(image_desc)
-            else:
-                descriptions[image_id] = [image_desc]
+        except Exception as e: 
+            print("Exception got :- \n",e)
 
-        return descriptions
-
-    def __getitem__(self, index: int) -> (str, list):
+        self.dictionary = descriptions
+#         pickle.dump(self.dictionary,open("full_dictionary.pkl","wb"))
+        return self.dictionary
+        
+    def __getitem__(self, index: int) -> (str, list, list):
         """
         :param index:
-        :return: (img_file_path, list_of_captions)
+        :return: (img_file_path, list_of_captions, list_of_wav_files)
         """
-        return self.id_to_file[self.ids[index]], self.id_to_captions[self.ids[index]]
+        image_id = self.ids[index]
+        return self.dictionary[image_id]['im_path'], self.dictionary[image_id]['descs'], self.dictionary[image_id]['wavs']
 
     def __len__(self):
         return len(self.ids)
