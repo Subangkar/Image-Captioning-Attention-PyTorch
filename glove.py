27e69c5fde61c8c53e2b3003812c1160491e6c0c
Subangkar <subangkar.karmaker@gmail.com>
2020-12-13 02:42:59 +0600

    glove embedding matrix

---
+++ glove.py
@@ -0,0 +1,26 @@
+import os
+import numpy as np
+
+
+# GLOVE_DIR = path for glove.6B.100d.txt
+def glove_dictionary(GLOVE_DIR, dim=200):
+    embeddings_index = {}
+    f = open(os.path.join(GLOVE_DIR, f'glove.6B.{dim}d.txt'), encoding="utf8")
+    for line in f:
+        values = line.split()
+        word = values[0]
+        coefs = np.asarray(values[1:], dtype='float32')
+        embeddings_index[word] = coefs
+    f.close()
+    return embeddings_index
+
+
+def embedding_matrix_creator(embedding_dim, word2idx, GLOVE_DIR='data/glove.6B/'):
+    embeddings_index = glove_dictionary(GLOVE_DIR=GLOVE_DIR, dim=embedding_dim)
+    embedding_matrix = np.zeros((len(word2idx) + 1, embedding_dim))
+    for word, i in word2idx.items():
+        embedding_vector = embeddings_index.get(word.lower())
+        if embedding_vector is not None:
+            # words not found in embedding index will be all-zeros.
+            embedding_matrix[i] = embedding_vector
+    return embedding_matrix
--- train_keras.py
+++ train_keras.py
@@ -37,7 +37,9 @@ vocab_size, max_len
 # %%
 
 samples_per_epoch = sum(map(lambda cap: len(cap.split()) - 1, caps))
-samples_per_epoch
+samples_per_epoch_val = sum(map(lambda cap: len(cap.split()) - 1, dset.add_start_end_seq(val_d)))
+samples_per_epoch_test = sum(map(lambda cap: len(cap.split()) - 1, dset.add_start_end_seq(test_d)))
+samples_per_epoch, samples_per_epoch_val, samples_per_epoch_test
 
 # %%
 
@@ -61,7 +63,11 @@ final_model.summary()
 
 BATCH_SIZE = 256
 MODEL_NAME = f'saved_models/IncepV3_bidirlstm_b{BATCH_SIZE}'
+NUM_EPOCHS = 50
 steps_per_epoch = int(math.ceil(samples_per_epoch / BATCH_SIZE))
+steps_per_epoch_val = int(math.ceil(samples_per_epoch_val / BATCH_SIZE))
+steps_per_epoch_test = int(math.ceil(samples_per_epoch_test / BATCH_SIZE))
+steps_per_epoch, steps_per_epoch_val, steps_per_epoch_test
 
 # %%
 
@@ -70,7 +76,7 @@ final_model.fit(
                          encoding_train=encoding_train, imgfilename_to_caplist_dict=train_d,
                          word2idx=word2idx, vocab_size=vocab_size, max_len=max_len),
     steps_per_epoch=steps_per_epoch,
-    epochs=50,
+    epochs=NUM_EPOCHS,
     callbacks=[
         keras.callbacks.ModelCheckpoint(
             f'{MODEL_NAME}''_ep{epoch:02d}_weights.h5',
@@ -78,8 +84,14 @@ final_model.fit(
         keras.callbacks.EarlyStopping(patience=10, monitor='loss'),
         keras.callbacks.ModelCheckpoint(f'{MODEL_NAME}''_best_train.h5', monitor='loss',
                                         save_best_only=True, mode='min'),
-    ])
-final_model.save(f"{MODEL_NAME}_ep{50}.h5")
+        keras.callbacks.ReduceLROnPlateau(monitor='loss', factor=0.5, patience=5, min_lr=1e-5),
+    ],
+    validation_data=dset.get_generator(batch_size=BATCH_SIZE, random_state=0,
+                                       encoding_train=encoding_valid, imgfilename_to_caplist_dict=val_d,
+                                       word2idx=word2idx, vocab_size=vocab_size, max_len=max_len),
+    validation_steps=steps_per_epoch_val
+)
+final_model.save(f"{MODEL_NAME}_ep{NUM_EPOCHS}.h5")
 
 # %%
 
